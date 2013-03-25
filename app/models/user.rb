fe89db92db5aa8f72d79e03454a4ef3e12590d14
Alain Ravet <alainravet@gmail.com>
2013-03-25 23:24:58 +0100

    authenticate with secure_password (see Railscast 270)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :login, :name, :password
 
-  validates_presence_of :name, :login, :password
+  has_secure_password
+
+  attr_accessible :login, :name, :password, :password_confirmation
+
+  validates_presence_of :name, :login
   validates_uniqueness_of :login
 end
