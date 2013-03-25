486f76611b018d92ef3a100928283aa51508e042
Alain Ravet <alainravet@gmail.com>
2013-03-25 21:39:10 +0100

    a User must have a :login, :name, and :password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :login, :name, :password
+
+  validates_presence_of :name
+  validates_presence_of :login
+  validates_presence_of :password
+
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_valid_user
+    assert User.new(valid_user_parameters).valid?
+  end
+
+  def test_user_must_have_a_name     ; assert !User.new(valid_user_parameters.except(:name    )).valid? end
+  def test_user_must_have_a_login    ; assert !User.new(valid_user_parameters.except(:login   )).valid? end
+  def test_user_must_have_a_password ; assert !User.new(valid_user_parameters.except(:password)).valid? end
+
+private
+  def valid_user_parameters
+    {:name => 'aname', :login => 'alogin', :password => 'apassword'}
+  end
+
 end
